3c74109a28c84660ec5e6e440da1ccd04c87087c
Bgrswxs <bgrswxswfgzyhz@gmail.com>
2018-02-11 23:10:54 +0800

    Delete play.py

--- play.py
+++
@@ -1,9 +0,0 @@
-import SpiderMan
-import time
-
-begin = time.time()
-url = 'http://bbs.csdn.net/forums/JavaScript'
-sm = SpiderMan.SpiderMan()
-sm.craw(url)
-end = time.time()
-print("\n耗时：{:.6} 秒".format(end-begin))
